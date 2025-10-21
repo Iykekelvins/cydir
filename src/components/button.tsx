fce954fe7861cf48c22dce44d593f6d0a6fddcf6
Kelvin Ochubili <kelvinochubili@gmail.com>
2025-10-21 09:01:56 +0100

    feat: add hover effect for buttons

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -12,13 +12,27 @@ export default function Button({ children, bg, onClick, className }: ButtonProps
 		<button
 			className={cn(
 				'flex items-center justify-center text-base font-medium',
-				'border border-solid rounded-full py-[max(1.1rem,_14px)] px-[max(1.25rem,_16px)]',
-				bg === 'lemon' && 'bg-lemon text-[#0A182D] border-[#EFEFEF]',
-				bg === 'blue' && 'bg-blue text-white border-[#192A4D99]',
+				`border border-solid rounded-full py-[max(1.1rem,_14px)] 
+				px-[max(1.25rem,_16px)] relative overflow-hidden`,
+				bg === 'lemon' &&
+					`bg-lemon text-[#0A182D] border-[#EFEFEF]
+					before:bg-blue before:absolute before:bottom-0
+					before:left-0 before:w-full before:h-0
+					before:rounded-full before:pointer-events-none
+					before:transition-all before:duration-300
+					before:ease-in-out hover:text-white
+					transition-all duration-300 ease-in-out
+					hover:before:h-full
+				`,
+				bg === 'blue' &&
+					`bg-blue text-white border-[#192A4D99]
+					transition-all duration-300 ease-in-out
+					hover:scale-95
+				`,
 				className
 			)}
 			onClick={onClick}>
-			{children}
+			<span className='z-[2]'>{children}</span>
 		</button>
 	);
 }
