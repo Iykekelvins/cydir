79e5c676fb3d3c7821175231faf920a5e0589919
Kelvin Ochubili <kelvinochubili@gmail.com>
2025-12-16 17:48:25 +0100

    feat: update video urls

--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -50,8 +50,8 @@ export const BREAKTHROUGH_ITEMS = [
 export const FEED = [
 	'https://res.cloudinary.com/dvgyi0ngj/video/upload/v1765878940/ig-1_ta5coi.mp4',
 	'https://res.cloudinary.com/dvgyi0ngj/video/upload/v1765878938/ig-2_f6n9vr.mp4',
-	'https://res.cloudinary.com/dvgyi0ngj/video/upload/v1765898797/ig-3_in8wti.mp4',
-	'https://res.cloudinary.com/dvgyi0ngj/video/upload/v1765898795/ig-4_vzqpte.mp4',
+	'https://res.cloudinary.com/dvgyi0ngj/video/upload/v1765903609/ig-3_makops.mp4',
+	'https://res.cloudinary.com/dvgyi0ngj/video/upload/v1765903609/ig-4_ln0tmt.mp4',
 ];
 
 export const FAQS = [
